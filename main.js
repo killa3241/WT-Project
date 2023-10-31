3012cd919cbe6eaaeef24bcb703a643356a286cc
AkshaySimha5 <74501906+AkshaySimha5@users.noreply.github.com>
2023-10-31 23:13:43 +0530

    Updated the img links to television products according to the images added

--- main.js
+++ main.js
@@ -5,62 +5,62 @@ let shopItemsData = [{
   name:"LG 4K UHD TV ",
   price:27990,
   desc:"LG 108 cm (43 inches) 4K Ultra HD Smart LED TV  (Dark Iron Gray):",
-  img:"television/lg.png"
+  img:"television/lg.jpg"
 },{
   id:"lg2",
   name:"LG  NanoCell TV ",
   price:36990,
   desc:"LG 108 cm (43 inches) 4K Ultra HD Smart NanoCell TV 43NANO73SQA (Ashed Blue)",
-  img:"television/lg2.png"
+  img:"television/lg2.jpg"
 },{
   id:"mi",
   name:"MI A Series HD TV",
   price: 10990,
   desc:"MI 80 cm (32 inches) A Series HD Ready Smart Google TV L32M8-5AIN (Black)",
-  img:"television/mi.png"
+  img:"television/mi.jpg"
 },{
     id:"mi2",
     name:"Xiaomi X Series",
     price: 24490,
     desc:"Xiaomi 108 cm (43 inches) X Series 4K Ultra HD Smart Android LED TV (Black)",
-    img:"television/mi2.png"
+    img:"television/mi2.jpg"
 },{
     id:"samsung",
     name:"Samsung Crystal iSmart",
     price: 63990,
     desc:"Samsung 163 cm (65 inches) Crystal iSmart 4K Ultra HD Smart LED TV",
-    img:"television/samsung.png"
+    img:"television/samsung.jpg"
 
 },{
     id:"samsung2",
     name:"Samsung Crystal 4K Neo",
     price: 28490,
     desc:"Samsung 108 cm (43 inches) Crystal 4K Neo Series Ultra HD Smart LED TV (Black)",
-    img:"television/samsung2.png"
+    img:"television/samsung2.jpg"
 },{
     id:"sony",
     name:"Sony Bravia 4K Ultra HD",
     price: 52990,
     desc:"Sony Bravia 139 cm (55 inches) 4K Ultra HD Smart LED Google TV (Black)",
-    img:"television/sony.png"
+    img:"television/sony.jpg"
 },{
     id:"sony2",
     name:"Sony Bravia 4K Ultra HD ",
     price: 81990,
     desc:"Sony Bravia 164 cm (65 inches) 4K Ultra HD Smart LED Google TV KD-65X74L (Black)",
-    img:"television/sony2.png"
+    img:"television/sony2.jpg"
 },{
     id:"toshiba",
     name:"TOSHIBA 4K Ultra HD",
     price:45990,
     desc:"TOSHIBA 139 cm (55 inches) 4K Ultra HD Smart Mini LED TV 55M650MP (Black)",
-    img:"television/toshiba.png"
+    img:"television/toshiba.jpg"
 },{
     id:"toshiba2",
     name:"TOSHIBA V Series HD",
     price:11490,
     desc:"TOSHIBA 80 cm (32 inches) V Series HD Ready Smart Android LED TV 32V35MP (Black)",
-    img:"television/toshiba2.png"
+    img:"television/toshiba2.jpg"
 }]
 
 let generateShop=()=>{
