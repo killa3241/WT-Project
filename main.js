b619691505cd5ad4c75c1f121fd27a427b863fa3
AkshaySimha5 <74501906+AkshaySimha5@users.noreply.github.com>
2023-11-01 23:40:38 +0530

    Added laptops' product description

--- main.js
+++ main.js
@@ -61,6 +61,66 @@ let shopItemsData = [{
     price:11490,
     desc:"TOSHIBA 80 cm (32 inches) V Series HD Ready Smart Android LED TV 32V35MP (Black)",
     img:"television/toshiba2.jpg"
+},{
+    id:"aceraspire",
+    name:"Acer Aspire 5",
+    price:51990,
+    desc:"Acer Aspire 5 Gaming Laptop Intel Core i5 12th gen (16 GB/512 GB SSD/Win11 Home/4GB Graphics/RTX 2050) A515-57G (15.6 FHD Display, 1.8 Kg)",
+    img:"laptop/AcerAspire5.png"
+},{
+    id:"applemacbook",
+    name:"Apple MacBook Air",
+    price:69990,
+    desc:"Apple MacBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver",
+    img:"laptop/AppleMacbookAirM1.png"
+},{
+    id:"asusrog",
+    name:"ASUS Rog Zephyrus M16",
+    price:214990,
+    desc:"ASUS Rog Zephyrus M16 (2022) 16 Inches 2K Qhd 165Hz/3Ms, Intel Core I9-12900H 12Th Gen, RTX 3080 Ti 16Gb Graphics, Gaming Laptop (32Gb/2Tb Ssd/Windows 11 Home/Office 2021/Black/2 Kg)",
+    img:"laptop/ASUSRogZephyrusM16(2022).jpg"
+},{
+    id:"asusvivo",
+    name:"ASUS Vivobook 15",
+    price:61990,
+    desc:"ASUS Vivobook 15 (2023), Intel Core i5-1335U 13th Gen, 15.6 (39.62 cm) FHD, Thin and Light Laptop (16GB/512GB/Win11/Office 2021/Fingerprint/Silver/1.7 kg)",
+    img:"laptop/ASUSVivobook15.png"
+},{
+    id:"dellg15",
+    name:"Dell G15 5520",
+    price:70990,
+    desc:"Dell G15 5520 Gaming Laptop, Intel i5-12500H,16GB DDR5,512GB SSD,NVIDIA RTX 3050 (4GB GDDR6),15.6(39.62cm) FHD WVA AG 120Hz 250 nits, Backlit KB Orange, Win 11 + MSO'21,Dark Shadow Grey,2.81kg",
+    img:"laptop/DellG155520.png"
+},{
+    id:"dellalien",
+    name:"Dell Gaming Alienware m15 R7",
+    price:109990,
+    desc:"Dell Gaming Alienware m15 R7 Laptop, AMD Ryzen 7-6800H/ 16GB/ 512GB SSD/NVIDIA RTX 3060 (6GB GDDR6)/ 15.6 (39.62Cms) FHD 165 Hz/Win 11 + MSO'21",
+    img:"laptop/DellGamingAlienwarem15R7.png"
+},{
+    id:"hpenvy",
+    name:"HP Envy x360",
+    price:113850,
+    desc:"HP Envy x360 12th Gen Intel Core i7-13.3 inch(33.8 cm) OLED Multi-Touch Gorilla Glass 2-in-1 Laptop(16GB RAM/512GB SSD/400 nits/Win 11/Intel Iris Xe Graphics/Alexa/B&O/Zenvo Pen/MSO/1.32Kg)",
+    img:"laptop/HPEnvyx360.jpg"
+},{
+    id:"lenovothinkpad",
+    name:"Lenovo ThinkPad E14",
+    price:97990,
+    desc:"Lenovo ThinkPad E14 Intel Core i7 12th Gen 14 FHD IPS Thin and Light Laptop (16GB RAM/1TB SSD/Windows 11 Home/MS Office H&S 2021/Backlit Keybaord/FPR/Black/1.59 kg)",
+    img:"laptop/LenovoThinkPadE14.jpg"
+},{
+    id:"msi",
+    name:"MSI Crosshair 15",
+    price:125990,
+    desc:"MSI Crosshair 15 B12UGZ - Intel i7 12700H-3.5 GHz Max Boost 4.7 GHz, 16GB DDR4, 1TB NVMe SSD, Nvidia RTX 3070-8GB GDDR6, 15.6” IPS-Level QHD 2560 * 1440-165Hz, Backlit, Windows 11 Home, 2 Yr Warranty",
+    img:"laptop/MSICrosshair15B12UGZ.jpg"
+},{
+    id:"lenovolegion",
+    name:"Lenovo Legion 5 Pro",
+    price:119990,
+    desc:"Lenovo Legion 5 Pro AMD Ryzen 7 5800H 16 (40.64cm) QHD IPS 165Hz 500Nits Gaming Laptop (32GB/1TB SSD/Win 11/Office 2021/NVIDIA RTX 3070 8GB/Alexa/3 Month Game Pass/Storm Grey/2.45Kg)",
+    img:"laptop/LenovoLegion5Pro.jpg"
 }]
 
 let generateShop=()=>{
