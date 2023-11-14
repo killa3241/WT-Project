33be1a62fce836bfcd98452ae8537ad348d5354a
AkshaySimha5 <74501906+AkshaySimha5@users.noreply.github.com>
2023-11-14 18:04:21 +0530

    Cart-number dynamic update

--- shopping-cart/src/shop.js
+++ shopping-cart/src/shop.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import './style.css';
 import ShopItem from './Shopitem';
 
-const Shop = ({ shopItemsData }) => {
+function Shop({ shopItemsData, incrementCart,decrementCart}) {
+  const rows = [];
+  for (let i = 0; i < shopItemsData.length; i += 5) {
+    rows.push(shopItemsData.slice(i, i + 5));
+  }
+
   return (
-    <div id="shop">
-      {shopItemsData.map((item) => (
-        <ShopItem key={item.id} item={item} />
+    <div className="shop">
+      {rows.map((row, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {row.map((item) => (
+            <ShopItem key={item.id} item={item} incrementCart={incrementCart} decrementCart={decrementCart}/>
+          ))}
+        </div>
       ))}
     </div>
   );
-};
+}
 
 export default Shop;
