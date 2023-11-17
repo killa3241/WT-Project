cbe2c7677a1ca670b6c2baf6d5957b99835961f3
AkshaySimha5 <74501906+AkshaySimha5@users.noreply.github.com>
2023-11-17 08:58:36 +0530

    Update CartNavbar.js

--- shopping-cart/src/CartNavbar.js
+++ shopping-cart/src/CartNavbar.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import {AiFillCaretDown} from 'react-icons/ai'
+import React, { useState } from 'react';
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+const ShopItem = ({ item,incrementCart,decrementCart}) => {
+  const { id, name, price, desc, img } = item;
+  const [quantity, setQuantity] = useState(0);
 
-function Navbar() {
+  const increment = () => {
+    setQuantity(quantity + 1);
+    incrementCart();
+  };
+
+  const decrement = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+      decrementCart();
+    }
+  };
+
   return (
-    <nav className="navbar">
-      <ul>
-      <a href="">Home</a>
-      <a href="">About</a>
-      <div className="dropdown">
-    <button class="dropbtn">Contact</button>
-    <div class="dropdown-content">
-      <a target='_blank' href="https://www.linkedin.com/in/akshay-simha-l-n-4a492a246/">AKSHAY SIMHA L N email: akshaysimha04@gmail.com</a>
-      <a target='_blank'  href="https://www.linkedin.com/in/anikait-mohan-937288227/">ANIKAIT NANJUNDAPPA email: anikaitmohan04@gmail.com</a>
-      <a target='_blank' href="https://www.linkedin.com/in/ashish-chandra-293355292/">ASHISH CHANDRA K email: ashish.chandra022@gmail.com</a>
-      <a  target='_blank'href="">ANISH D B email:@gmail.com</a>
-    </div>
-    </div>
-    <div className="dropdown">
-    <button class="dropbtn">Sort By
-      <AiFillCaretDown className='drop-down-icon'/>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Television</a>
-      <a href="#">Laptop</a>
-      <a href="#">Home Appiances</a>
-      <a href="#">Electronic Gadgets</a>
-      <a href="#">Gaming</a>
-    </div>
-  </div>
-        
-      </ul>
-    </nav>
+    <div className="item">
+      <img width="220" src={img} alt={name} />
+      <div className="details">
+        <h3>{name}</h3>
+        <p>{desc}</p>
+        <div className="price-quantity">
+          <h2>₹ {price}</h2>
+          <div className="buttons">
+            <AiFillMinusCircle className="plusminusicons" onClick={decrement} />
+            <div className="quantity">{quantity}</div>
+            <AiFillPlusCircle className="plusminusicons" onClick={increment} />
+          </div>
+        </div>
+      </div>
+      </div>
   );
-}
+};
 
-export default Navbar;
+export default ShopItem;
