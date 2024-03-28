6dd41fa0411b4af7f456c8f4ca211669d8b5e5f1
Mr.Man <113169604+killa3241@users.noreply.github.com>
2024-03-28 09:07:58 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/user')
+
+const app = express()
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+app.use('/api/user', userRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
